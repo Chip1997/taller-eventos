92255c7a044865265b07c61de5cd0977d1eb5256
Javier Idiarte <99035124+Chip1997@users.noreply.github.com>
2023-08-05 19:37:07 -0300

    stopPropagation
    utilización de stopPropagation en JS y optimización del onclick en el html a usar el addEventListener  en el js

--- eventos.js
+++ eventos.js
@@ -3,3 +3,8 @@ const div = document.querySelector("#divBtn");
 div.addEventListener("click", () => {alert("Hola! Soy el div");
 });
 
+document.getElementById('hiBtn').addEventListener('click', function(event) {
+    event.stopPropagation();
+    {alert("¡Hola!");
+}
+  });
