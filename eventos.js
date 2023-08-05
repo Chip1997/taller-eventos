4ab132517871d595295a3b3054fe13dc9b307622
Javier Idiarte <99035124+Chip1997@users.noreply.github.com>
2023-08-05 18:24:30 -0300

    borro os cambios por que iban en otra branch

--- eventos.js
+++ eventos.js
@@ -3,8 +3,3 @@ const div = document.querySelector("#divBtn");
 div.addEventListener("click", () => {alert("Hola! Soy el div");
 });
 
-document.getElementById('hiBtn').addEventListener('click', function(event) {
-    event.stopPropagation();
-    {alert('¡Hola!');
-}
-  });
