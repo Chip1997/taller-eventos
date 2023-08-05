81dedfdb866a695e5275da42e1f3112ed31ddf17
Javier Idiarte <99035124+Chip1997@users.noreply.github.com>
2023-08-05 18:21:50 -0300

    event.stopPropagation
    Utilice  el stopPropagation para evitar que al clickear el boton tambien saliera la alerta del div y cambie el código html a donde el onclick deja de estar ahi y uso el addEventListener desde el js

--- eventos.js
+++ eventos.js
@@ -2,3 +2,9 @@ const div = document.querySelector("#divBtn");
 
 div.addEventListener("click", () => {alert("Hola! Soy el div");
 });
+
+document.getElementById('hiBtn').addEventListener('click', function(event) {
+    event.stopPropagation();
+    {alert('¡Hola!');
+}
+  });
